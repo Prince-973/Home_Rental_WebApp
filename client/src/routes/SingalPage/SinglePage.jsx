d099881b3a1427f72176d7455f2a96d2ed91ba7a
Prince-973 <prinsvaghasiya.it21@scet.ac.in>
2025-02-17 16:25:07 +0530

    create the info and image/imageSlider part of the singal page

--- client/src/routes/SingalPage/SinglePage.jsx
+++ client/src/routes/SingalPage/SinglePage.jsx
@@ -1,4 +1,5 @@
 import Slider from "../../components/slider/Slider";
+import { singlePostData, userData } from "../../lib/dummydata";
 import "./SinglePage.scss";
 
 function SinglePage() {
@@ -6,7 +7,24 @@ function SinglePage() {
     <div className="singlePage">
       <div className="details">
         <div className="wrapper">
-          <Slider />
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">₹{singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">{singlePostData.description}</div>
+          </div>
         </div>
       </div>
       <div className="features">
